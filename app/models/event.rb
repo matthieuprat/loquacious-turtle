3967e00551069a6577b0e566c10a7de0a5006c1e
Matthieu Prat <matthieu.prat@dailymotion.com>
2015-11-18 17:24:32 +0100

    event: compute availabilities

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,33 @@
+require 'set'
+
 class Event < ActiveRecord::Base
+  def self.availabilities(date_from)
+    pitch = 30.minutes
+    date_from = date_from.to_date
+
+    # Retrieve all events affecting `date_from` and the 6 following days.
+    events = Event.where('starts_at < ?', date_from + 7.days)
+                  .where('starts_at >= ?', date_from)
+                  .order(starts_at: :asc)
+
+    # Discretize events and group them by kind.
+    slots = { 'opening' => SortedSet.new, 'appointment' => Set.new }
+    events.each do |event|
+      slot = event.starts_at
+      while slot < event.ends_at do
+        slots[event.kind] << slot
+        slot += pitch
+      end
+    end
+
+    # Resolve available slots and group them by day.
+    availabilities = (slots['opening'] - slots['appointment'])
+                     .group_by { |slot| slot.to_date }
+
+    # Return available slots for `date_from` and the 6 following days.
+    date_from.upto(date_from + 6.days).map do |date|
+      slots = availabilities[date] || []
+      { date: date, slots: slots.map { |s| s.strftime('%-H:%M') } }
+    end
+  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "basic events" do
+    Event.create kind: 'opening', starts_at: DateTime.parse('2014-08-11 09:30'), ends_at: DateTime.parse('2014-08-11 12:30')
+    Event.create kind: 'appointment', starts_at: DateTime.parse('2014-08-11 10:30'), ends_at: DateTime.parse('2014-08-11 11:30')
+
+    availabilities = Event.availabilities DateTime.parse('2014-08-10')
+    assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
+    assert_equal [], availabilities[0][:slots]
+    assert_equal Date.new(2014, 8, 11), availabilities[1][:date]
+    assert_equal ['9:30', '10:00', '11:30', '12:00'], availabilities[1][:slots]
+    assert_equal Date.new(2014, 8, 16), availabilities[6][:date]
+    assert_equal 7, availabilities.length
+  end
 end
