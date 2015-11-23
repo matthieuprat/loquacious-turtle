63a0c8cc27c1f95d2046cdb34ae3c37b9a79d82f
Matthieu Prat <matthieu.prat@dailymotion.com>
2015-11-23 13:28:05 +0100

    event: compute availabilities

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,48 @@
+require 'set'
+
 class Event < ActiveRecord::Base
+  def self.availabilities(date, duration = 30.minutes)
+    duration = duration.seconds
+    date = date.to_date
+
+    # Retrieve all events affecting `date` and the 6 following days.
+    events = Event.where('starts_at < ?', date + 7.days)
+                  .where('starts_at >= ?', date)
+                  .order(starts_at: :asc)
+                  .to_a
+
+    # Reshape events.
+    events.map! do |e|
+      starts_at = e.starts_at.to_datetime
+      ends_at   = e.ends_at.to_datetime
+      [e.kind.to_sym, starts_at..ends_at]
+    end
+
+    # Compute availability periods.
+    avails = [] # Array of date ranges.
+    avail = DateTime.new(0)..DateTime.new(0) # "Fake" availability for initialization.
+    events.sort_by { |_, e| e.begin }.each do |kind, event|
+      case kind
+      when :opening
+        if event.begin > avail.end
+          avails << avail
+          avail = event
+        end
+      when :appointment
+        avails << (avail.begin..event.begin)
+        avail = event.end..avail.end
+      end
+    end
+    avails << avail
+    avails.shift # Slash the fake availability.
+
+    # Chunk availability periods into slots and group them by day.
+    slots = avails.map { |a| a.begin.step(a.end - duration, duration.fdiv(1.day)) }
+                  .flat_map(&:to_a)
+                  .group_by(&:to_date)
+
+    date.upto(date + 6.days).map do |date|
+      { date: date, slots: (slots[date] || []).map { |s| s.strftime('%-H:%M') } }
+    end
+  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "no event" do
+    date = DateTime.parse('2014-08-10')
+    availabilities = Event.availabilities date
+    date.upto(date + 6.days).each_with_index do |date, i|
+      assert_equal date, availabilities[i][:date]
+      assert_equal [], availabilities[i][:slots]
+    end
+    assert_equal 7, availabilities.length
+  end
+
+  test "opening" do
+    Event.create kind: 'opening', starts_at: DateTime.parse('2014-08-11 09:30'), ends_at: DateTime.parse('2014-08-11 11:30')
+    Event.create kind: 'opening', starts_at: DateTime.parse('2014-08-12 09:15'), ends_at: DateTime.parse('2014-08-12 11:00')
+
+    availabilities = Event.availabilities DateTime.parse('2014-08-10')
+    assert_equal ['9:30', '10:00', '10:30', '11:00'], availabilities[1][:slots]
+    assert_equal ['9:15', '9:45', '10:15'], availabilities[2][:slots]
+  end
+
+  test "opening and appointment" do
+    Event.create kind: 'opening', starts_at: DateTime.parse('2014-08-11 09:30'), ends_at: DateTime.parse('2014-08-11 12:30')
+    Event.create kind: 'appointment', starts_at: DateTime.parse('2014-08-11 10:30'), ends_at: DateTime.parse('2014-08-11 11:30')
+
+    availabilities = Event.availabilities DateTime.parse('2014-08-10')
+    assert_equal ['9:30', '10:00', '11:30', '12:00'], availabilities[1][:slots]
+
+    Event.create kind: 'appointment', starts_at: DateTime.parse('2014-08-11 09:30'), ends_at: DateTime.parse('2014-08-11 10:30')
+
+    availabilities = Event.availabilities DateTime.parse('2014-08-10')
+    assert_equal ['11:30', '12:00'], availabilities[1][:slots]
+
+    Event.create kind: 'appointment', starts_at: DateTime.parse('2014-08-11 12:00'), ends_at: DateTime.parse('2014-08-11 12:30')
+
+    availabilities = Event.availabilities DateTime.parse('2014-08-10')
+    assert_equal ['11:30'], availabilities[1][:slots]
+  end
 end
