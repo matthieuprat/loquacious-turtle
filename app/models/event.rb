adffe6ee56f9baea9d73ea7c84c5fa6226330d36
Matthieu Prat <matthieu.prat@dailymotion.com>
2015-11-26 18:43:05 +0100

    event: use a fixed precision
    There was no real value in computing a precision each day (we use to
    call it 'pitch'). Let's just fix this precision to 5 minutes to make the
    implementation more straightforward.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,15 @@
 require 'set'
 
 class Event < ActiveRecord::Base
+  @@precision = 5.minutes
+
   def self.availabilities(date, duration = 30.minutes)
     date = date.to_date
 
     events = self.fetch(date, date + 7.days)
     slots = self.discretize(events, duration)
+
+    slots = slots.group_by { |s| s.to_date }
 
     # Return available slots for `date` and the 6 following days.
     date.upto(date + 6.days).map do |date|
@@ -32,44 +36,33 @@ class Event < ActiveRecord::Base
     end
   end
 
-  # Extract available slots from events, grouped by day.
+  # Extract available slots from events.
   def self.discretize(events, duration)
-    availabilities = {}
-    events.group_by { |e| e.starts_at.to_date }.map do |date, events|
-      # Find the highest suitable pitch to discretize events.
-      first = events[0].starts_at
-      pitch = events.reduce(duration / 1.minute) { |pitch, event|
-        [event.starts_at, event.ends_at].map { |t| pitch.gcd((t - first).to_i / 60) }.min
-      }.minutes
+    precision = [duration, @@precision].min
 
-      # Discretize events and group them by kind.
-      slots = { 'opening' => SortedSet.new, 'appointment' => Set.new }
-      events.each do |event|
-        slot = event.starts_at
-        while slot < event.ends_at do
-          slots[event.kind] << slot
-          slot += pitch
-        end
+    slots = { 'opening' => SortedSet.new, 'appointment' => Set.new }
+    events.each do |e|
+      from, to = [e.starts_at, e.ends_at].map do |d|
+        Time.at((d.to_f / precision).round * precision).to_datetime.utc
       end
 
-      # Resolve availabilities.
-      slots = (slots['opening'] - slots['appointment']).to_a
-
-      if (duration > pitch)
-        n = duration / pitch # Number of slots needed to cover `duration`.
-
-        # "Merge" slots:
-        #   1. Split slots in chunks of contiguous slots.
-        #   2. Truncate chunks so that their sizes are a multiple of `n`.
-        #   3. Pick a slot every `n` slots from each chunk.
-        slots = slots.slice_when { |slot, next_slot| next_slot - slot > pitch }          # (1)
-                     .map { |slots| slots.first(slots.size - slots.size % n) }           # (2)
-                     .flat_map { |slots| slots.select.with_index { |_, i| i % n == 0 } } # (3)
-      end
-
-      availabilities[date] = slots
+      slots[e.kind].merge(from.step(to - precision, precision.to_f / 1.day))
     end
 
-    availabilities
+    # Resolve availabilities.
+    slots = (slots['opening'] - slots['appointment']).to_a
+
+    if (duration > precision)
+      # "Merge" slots:
+      #   1. Split slots in chunks of contiguous slots.
+      #   2. Truncate chunks so that their sizes are a multiple of `n`.
+      #   3. Pick a slot every `n` slots from each chunk.
+      n = duration / precision # Number of slots needed to cover `duration`.
+      slots.slice_when { |slot, next_slot| (next_slot - slot).days > precision } # (1)
+           .map { |slots| slots.first(slots.size - slots.size % n) }             # (2)
+           .flat_map { |slots| slots.select.with_index { |_, i| i % n == 0 } }   # (3)
+    else
+      slots
+    end
   end
 end
