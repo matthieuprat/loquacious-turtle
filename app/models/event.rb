ec3a804da0ad0748ae0cde566bb862ff24e4818c
Matthieu Prat <matthieu.prat@dailymotion.com>
2015-11-26 14:43:18 +0100

    event: refactor
    Move availibity event fetching and availability period computation to
    their own method.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,36 +1,52 @@
 class Event < ActiveRecord::Base
   def self.availabilities(date, duration = 30.minutes)
-    duration = duration.fdiv(1.day)
+    duration = duration.to_f / 1.day
     date = date.to_date
 
-    # Retrieve all events affecting `date` and the 6 following days.
-    events = Event.where('starts_at < ?', date + 7.days)
-                  .where('starts_at >= ? OR weekly_recurring = ?', date, true)
+    events = self.fetch(date, date + 7.days)
+    avails = self.compute_availabilities(events)
+
+    # Chunk availability periods into slots and group them by day.
+    slots = avails.map { |a| a.begin.to_datetime.step(a.end.to_datetime - duration, duration) }
+                  .flat_map(&:to_a)
+                  .group_by(&:to_date)
+
+    date.upto(date + 6.days).map do |date|
+      { date: date, slots: (slots[date] || []).map { |s| s.strftime('%-H:%M') } }
+    end
+  end
+
+  private
+
+  # Retrieve events affecting a date range.
+  def self.fetch(from, to)
+    events = Event.where('starts_at < ?', to)
+                  .where('starts_at >= ? OR weekly_recurring = ?', from, true)
                   .order(starts_at: :asc)
-                  .to_a
 
-    # Reshape events.
-    events.map! do |e|
-      starts_at = e.starts_at.to_datetime
-      ends_at   = e.ends_at.to_datetime
-      if e.weekly_recurring
+    events.map do |event|
+      if event.weekly_recurring
         # Shift event dates.
-        offset = ((date - 1 - starts_at.to_date).to_i / 7 + 1).weeks
-        starts_at += offset
-        ends_at   += offset
+        offset = ((from - 1 - event.starts_at.to_date).to_i / 7 + 1).weeks
+        event.starts_at += offset
+        event.ends_at   += offset
       end
-      [e.kind.to_sym, starts_at..ends_at]
+      event
     end
+  end
 
-    events.sort_by! { |_, e| e.begin }
+  # Compute an array of date ranges representing availability periods.
+  def self.compute_availabilities(events)
+    return [] if events.empty?
 
-    # Compute availability periods.
+    # Reshape events and sort them by start date.
+    events = events.map { |e| [e.kind.to_sym, e.starts_at..e.ends_at] }
+                   .sort_by { |_, r| r.begin }
+
     avails = [] # Array of date ranges representing availability periods.
-    avail = nil # Next (candidate) availability period.
-    unless events.empty?
-      kind, avail = events.shift
-      avail = avail.end..avail.end if kind == :appointment
-    end
+    kind, avail = events.shift
+    avail = avail.end..avail.end if kind == :appointment
+
     events.each do |kind, event|
       case kind
       when :opening
@@ -46,15 +62,6 @@ class Event < ActiveRecord::Base
         avail = event.end..[avail.end, event.end].max
       end
     end
-    avails << avail if avail
-
-    # Chunk availability periods into slots and group them by day.
-    slots = avails.map { |a| a.begin.step(a.end - duration, duration) }
-                  .flat_map(&:to_a)
-                  .group_by(&:to_date)
-
-    date.upto(date + 6.days).map do |date|
-      { date: date, slots: (slots[date] || []).map { |s| s.strftime('%-H:%M') } }
-    end
+    avails << avail
   end
 end
